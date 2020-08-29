9260c1e5314d61414616977e2d34436fae75f413
Milad Awad <millad.99.awad@gmail.com>
2020-08-30 00:11:57 +0300

    Added background image to the picking_screen

--- lib/screens/picking_screen.dart
+++ lib/screens/picking_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:picki/screens/result_screen.dart';
@@ -30,106 +31,127 @@ class _PickingScreenState extends State<PickingScreen> {
   final secondPlayerController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Text(
-              widget.screenNumber == 1 ? '1st Team' : '2nd Team',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50.0),
-              child: TextFormField(
-                controller: widget.screenNumber == 1
-                    ? firstPlayerController
-                    : secondPlayerController,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.person_outline),
-                  hintText: "Player's Name",
-                ),
-              ),
-            ),
-            Container(
-              height: 250.0,
-              child: chose
-                  ? Image.asset('images/Teams/$team.png')
-                  : Icon(
-                      Icons.check_box_outline_blank,
-                      size: 200.0,
-                    ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                RaisedButton(
-                  color: Color(0xFF00BCD4),
-                  child: Text(
-                    'Random',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      team = teamsNamesList[
-                          Random().nextInt(teamsNamesList.length)];
-                      chose = true;
-                    });
-                  },
-                ),
-                RaisedButton(
-                  color: Color(0xFF00BCD4),
-                  child: Text(
-                    'Manual',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            Divider(),
-            RaisedButton(
-              color: Color(0xFF00BCD4),
-              child: Text(
-                'Continue',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                var whereTo;
-                if (widget.screenNumber == 1) {
-                  whereTo = PickingScreen(
-                    screenNumber: 2,
-                    firstTeamName: team,
-                    firstPlayerName: firstPlayerController.text,
-                  );
-                } else if (widget.screenNumber == 2) {
-                  whereTo = ResultScreen(
-                    firstTeamName: widget.firstTeamName,
-                    secondTeamName: team,
-                    firstPlayerName: widget.firstPlayerName,
-                    secondPlayerName: secondPlayerController.text,
-                  );
-                }
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => whereTo,
-                  ),
-                );
-              },
-            ),
-          ],
+    return Stack(children: <Widget>[
+      Container(
+        constraints: BoxConstraints.expand(),
+        child: Image.asset(
+          'images/backgroundImage.jpg',
+          fit: BoxFit.cover,
         ),
       ),
-    );
+      Positioned.fill(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 0.5,
+            sigmaY: 0.5,
+          ),
+          child: Scaffold(
+            backgroundColor: Colors.black12,
+            body: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40.0),
+                    child: Text(
+                      widget.screenNumber == 1 ? '1st Team' : '2nd Team',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                    child: TextFormField(
+                      controller: widget.screenNumber == 1
+                          ? firstPlayerController
+                          : secondPlayerController,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.person_outline),
+                        hintText: "Player's Name",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 250.0,
+                    child: chose
+                        ? Image.asset('images/Teams/$team.png')
+                        : Icon(
+                            Icons.check_box_outline_blank,
+                            size: 200.0,
+                          ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      RaisedButton(
+                        color: Color(0xFF00BCD4),
+                        child: Text(
+                          'Random',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            team = teamsNamesList[
+                                Random().nextInt(teamsNamesList.length)];
+                            chose = true;
+                          });
+                        },
+                      ),
+                      RaisedButton(
+                        color: Color(0xFF00BCD4),
+                        child: Text(
+                          'Manual',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                  Divider(),
+                  RaisedButton(
+                    color: Color(0xFF00BCD4),
+                    child: Text(
+                      'Continue',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      var whereTo;
+                      if (widget.screenNumber == 1) {
+                        whereTo = PickingScreen(
+                          screenNumber: 2,
+                          firstTeamName: team,
+                          firstPlayerName: firstPlayerController.text,
+                        );
+                      } else if (widget.screenNumber == 2) {
+                        whereTo = ResultScreen(
+                          firstTeamName: widget.firstTeamName,
+                          secondTeamName: team,
+                          firstPlayerName: widget.firstPlayerName,
+                          secondPlayerName: secondPlayerController.text,
+                        );
+                      }
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => whereTo,
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    ]);
   }
 }
